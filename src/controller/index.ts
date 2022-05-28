686a7f8b53ebaf9bb7f0baab7775e2f2163fadc3
zhuonan <422914219@qq.com>
2022-05-28 17:37:14 +0800

    保存controller

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,15 +1,70 @@
-import { Controller, Get, Route } from "tsoa";
+import { Controller, Get, Path, Route } from "tsoa";
 
+/**
+ * excel文档
+ */
 interface Excel {
   id: number;
   name: string;
-  version: number;
+  version: Version;
+  versions: Version[];
+  sheets: Sheet[];
+}
+
+/**
+ * 文档版本
+ */
+interface Version {
+  id: number;
+  author: string;
+  excelName: string;
+  sheets: Sheet[];
+}
+
+/**
+ * sheet工作簿
+ */
+interface Sheet {
+  id: number;
+  name: string;
+  data: string;
 }
 
 @Route("excels")
 export class RouteController extends Controller {
+  /**
+   * 获取excel列表
+   * @summary 获取excel列表
+   */
   @Get()
   public async getExcels(): Promise<Excel[]> {
-    return [{ id: 1, name: "haha", version: 2 }];
+    return [{ id: 1, name: "haha", version: 2, history: [] }];
+  }
+
+  /**
+   * 通过excelid检索文档信息
+   * @param excelId excel唯一标示
+   * @summary 获取文档信息
+   */
+  @Get("{excelId}")
+  public async getExcel(@Path() excelId: number): Promise<Excel> {
+    return { id: 1, name: "haha", version: 2, history: [] };
+  }
+
+  /**
+   * 根据excelid+version获取版本信息
+   * @returns
+   */
+  @Get("{excelId}/{version}")
+  public async getExcelByVersion() {
+    return;
+  }
+
+  /**
+   * 获取sheet数据
+   */
+  @Get("sheet/{sheetId}")
+  public async getSheet(@Path() sheetId: number) {
+    return;
   }
 }
